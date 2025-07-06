ad1243fc691771ab0c38a8011715ec424df19a18
QXZ3ADQ <kabelo.maroga@bmwithub.co.za>
2025-07-06 16:17:50 +0200

    - Created QUOTE list component; -

---
+++ src/app/features/quotes/pages/quote-list/quote-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-list',
+  templateUrl: './quote-list.component.html',
+  styleUrls: ['./quote-list.component.scss']
+})
+export class QuoteListComponent {
+  constructor() { }
+}
