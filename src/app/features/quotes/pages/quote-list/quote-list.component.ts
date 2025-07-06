d81dc61366b467e3f476973c2141fa1e06daabe3
QXZ3ADQ <kabelo.maroga@bmwithub.co.za>
2025-07-06 17:36:42 +0200

    - Created QUOTE-from component. - Updated QUOTE list component.

--- src/app/features/quotes/pages/quote-list/quote-list.component.ts
+++ src/app/features/quotes/pages/quote-list/quote-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { QuoteService, QuoteWithCustomer } from '../../services/quote.service';
 
 @Component({
   selector: 'app-quote-list',
   templateUrl: './quote-list.component.html',
   styleUrls: ['./quote-list.component.scss']
 })
-export class QuoteListComponent {
-  constructor() { }
+export class QuoteListComponent implements OnInit {
+  quotes$: Observable<QuoteWithCustomer[]>;
+  displayedColumns = ['customerFullName', 'description', 'amount', 'status', 'createdDate'];
+
+  constructor(private quoteService: QuoteService) {
+    this.quotes$ = this.quoteService.getQuotesWithCustomers();
+  }
+
+  ngOnInit(): void {
+    // Data is loaded through the service
+  }
 }
